0f526ec088a3dfb779c69a43a9d3a8f9c668206a
Shubham Kadam <shubhamkadam@Shubhams-MacBook-Pro.local>
2023-06-14 19:10:33 +0530

    palindrome string done

--- Week-1/PrequisitesOfJS/Week-1-assignment/01-js/easy/expenditure-analysis.js
+++ Week-1/PrequisitesOfJS/Week-1-assignment/01-js/easy/expenditure-analysis.js
@@ -12,4 +12,5 @@ function calculateTotalSpentByCategory(transactions) {
   return [];
 }
 
+
 module.exports = calculateTotalSpentByCategory;
---
+++ Week-1/PrequisitesOfJS/Week-1-assignment/01-js/medium/palindrome.js
@@ -0,0 +1,16 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-palindrome`
+*/
+function isPalindrome(str) {
+  let revStr = str.split("").reverse().join("");
+
+  return revStr === str;
+}
+
+console.log(isPalindrome("abba"));
+
+// module.exports = isPalindrome;
