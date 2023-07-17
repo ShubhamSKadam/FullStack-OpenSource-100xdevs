b02a2ed76aae33f351a09b48cbc8c4dace209aab
CrypticCipher <crypticcipher@MacBook-Air.local>
2023-07-17 10:21:52 +0530

    middlewares in http requests

--- Week-2/IntroToBackend/index.js
+++ Week-2/IntroToBackend/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+// middleware function
+// function middleware1(req, res, next){
+//   console.log("middleware1 : " + req.headers.counter);
+//   next();
+// }
+
+// register middleware function
+// app.use(middleware1);
+
+app.use(bodyParser.json());
 
 function calculateSum(counter) {
   var sum = 0;
@@ -11,7 +23,9 @@ function calculateSum(counter) {
 }
 
 function handleFirstRequest(req, res) {
-  const counter = req.query.counter;
+  console.log(req.body);
+  const counter = req.body.counter;
+  // const counter = req.query.counter;
   var answer = calculateSum(counter);
   res.send(`The sum from 1 to ${counter} is ${answer}`);
 }
@@ -20,8 +34,8 @@ function createUser(req, res) {
   res.send("Hello World");
 }
 
-app.get("/handleSum", handleFirstRequest);
-app.get("/createUser", createUser);
+app.post("/handleSum", handleFirstRequest);
+// app.get("/createUser", createUser);
 function started() {
   console.log(`Example app listening on port ${port}`);
 }
