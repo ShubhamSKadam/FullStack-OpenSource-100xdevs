333e6be707730088fe3407f0e6d6a586a9cf4150
ShubhamSKadam <shubhamskadam99@outlook.com>
2023-10-13 12:07:27 +0530

    File Server Assignment Completed

---
+++ Week-2/Assignments/FileServer/fileServer.js
@@ -0,0 +1,77 @@
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+
+    - For any other route not defined in the server return 404
+
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+// Middleware to parse json data from request body
+app.use(express.json());
+const port = 3000;
+app.listen(port, () => {
+  console.log(`started listening on ${port}`);
+});
+
+const filesDirectory = "./files";
+
+app.get("/files", (req, res) => {
+  // Read the contents of the files in the directory
+  fs.readdir(filesDirectory, (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    // Send the list of files as a JSON response
+    res.status(200).json(files);
+  });
+});
+
+// Getting a specific file
+app.get("/files/:filename", (req, res) => {
+  const file = req.params.filename;
+  const filePath = `./files/${file}`;
+
+  // Check if the files exist
+  fs.access(filePath, fs.constants.R_OK, (err) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // File not found
+        res.status(404).send("File not found");
+      } else {
+        // File is not readable for some other reason
+        console.error("Error accessing the file", err);
+        res.status(500).send("Internal Server Error");
+      }
+    } else {
+      // Read the file and send it's content as the response
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        console.log("data", data);
+        if (err) {
+          console.error("Error reading the file", err);
+          res.status(500).send("Internal Server Error");
+        } else {
+          res.status(200).send(data);
+        }
+      });
+    }
+  });
+});
