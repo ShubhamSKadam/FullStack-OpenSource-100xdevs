3869ba05bd17663ccfca8eeb5988c2245d27a991
ShubhamSKadam <shubhamskadam99@outlook.com>
2023-11-14 16:12:48 +0530

    week-3 session 1 update

--- Week-3/Todo/todoServer.js
+++ Week-3/Todo/todoServer.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const path = require("path");
+const cors = require('cors');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 let todos = [];
 
@@ -66,10 +70,14 @@ app.delete("/todos/:id", (req, res) => {
   }
 });
 
-// for all other routes, return 404
-app.use((req, res, next) => {
-  res.status(404).send();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+
+// for all other routes, return 404
+// app.use((req, res, next) => {
+//   res.status(404).send();
+// });
 
 app.listen(3000);
 
